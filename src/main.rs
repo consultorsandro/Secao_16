4cd60efc081e5cf90586e43523f88cfd13044b90
Sandro Reis <consultorsandro@hotmail.com>
2025-06-15 10:24:03 -0300

    Class 262 Function remove

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use  std::collections::HashMap;
-fn main() { //Class 261
+use  std::collections::HashMap; // Class 261
+fn main() { //Class 262
+    let data = [("Bobby", 10), ("Alice", 20), ("Charlie", 30)];
+
+    let mut years_at_company: HashMap<&str, i32> = HashMap::from(data);
+    println!("Years at company: {:?}", years_at_company);
+
+    let charlie = years_at_company.remove("Charlie");
+    println!("Removed Ben: {:?}", charlie);
+    println!("{}", charlie.unwrap_or(0)); // unwrap_or() returns a default value if the key is not found
+    println!("Years at company after removal: {:?}", years_at_company);
+
+    let charlie = years_at_company.remove("charlie");
+    println!("Removed Charlie: {:?}", charlie);
+}
+/*
+//Class 261
    let mut menu: HashMap<String, f64> = HashMap::new();
     menu.insert(String::from("Steak"), 19.99);
     menu.insert(String::from("Tuna"), 19.99);
@@ -12,6 +27,4 @@ fn main() { //Class 261
     country_capitals.insert("Japan", "Tokyo");
 
     println!("Country Capitals: {:?}", country_capitals);
-
-}
- 
+ */
